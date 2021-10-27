b97d971bfd2f7a34770c7e301d1b540640b63428
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2021-10-27 22:31:34 +0200

    fix review service

---
+++ Infrastructure/Service/Review/IReviewService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Domain.DTO;
+
+namespace Infrastructure.Service
+{
+    public interface IReviewService
+    {
+        Task<IEnumerable<Review>> GetAllReviewsAsync();
+
+        Task<Review> GetReviewByIdAsync(string reviewId);
+
+        Task<Review> AddReviewAsync(ReviewDTO reviewDTO);
+
+        Task<Review> UpdateReviewAsync(ReviewDTO reviewDTO);
+
+        Task DeleteReviewAsync(string reviewId);
+    }
+}
---
+++ Infrastructure/Service/Review/ReviewService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Domain.DTO;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Service
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly ICosmosReadRepository<Review> _reviewReadRepository;
+        private readonly ICosmosWriteRepository<Review> _reviewWriteRepository;
+
+        public ReviewService(ICosmosReadRepository<Review> reviewReadRepository, ICosmosWriteRepository<Review> reviewWriteRepository)
+        {
+            _reviewReadRepository = reviewReadRepository;
+            _reviewWriteRepository = reviewWriteRepository;
+        }
+
+        public async Task<Review> AddReviewAsync(ReviewDTO reviewDTO)
+        {
+            Review review = new Review();
+            review.ReviewId = Guid.NewGuid();
+            review.ProductId = reviewDTO.ProductId;
+            review.Comments = reviewDTO.Comments;
+            return await _reviewWriteRepository.AddAsync(review);
+        }
+
+        public async Task DeleteReviewAsync(string reviewId)
+        {
+            Review review = await GetReviewByIdAsync(reviewId);
+            await _reviewWriteRepository.Delete(review);
+        }
+
+        public async Task<IEnumerable<Review>> GetAllReviewsAsync()
+        {
+            return await _reviewReadRepository.GetAll().ToListAsync();
+        }
+
+        public async Task<Review> GetReviewByIdAsync(string reviewId)
+        {
+            Guid id = Guid.Parse(reviewId);
+            return await _reviewReadRepository.GetAll().FirstOrDefaultAsync(r => r.ReviewId == id);
+        }
+
+        public async Task<Review> UpdateReviewAsync(ReviewDTO reviewDTO)
+        {
+            Review updateReview = new Review();
+            updateReview.Comments = reviewDTO.Comments;
+            return await _reviewWriteRepository.Update(updateReview);
+        }
+    }
+}
